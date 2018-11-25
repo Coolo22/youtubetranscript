6202a1a1884ad9d277e5ca8135a48a1682b2e391
Coolo22 <39219877+Coolo22@users.noreply.github.com>
2018-11-25 09:26:00 +0000

    Create GetWords.py

---
+++ GetWords.py
@@ -0,0 +1,78 @@
+from __future__ import unicode_literals
+import youtube_dl
+import speech_recognition as sr
+from os import path
+from pydub import AudioSegment
+from pytube import YouTube
+import os, sys
+import lxml, urllib, urllib.request
+from lxml import etree
+from urllib import request
+import time
+import ffmpeg
+import urllib.error
+import re
+import pipes
+import random
+
+
+def video_to_audio(fileName):
+	try:
+		file, file_extension = os.path.splitext(fileName)
+		file = pipes.quote(file)
+		video_to_wav = 'ffmpeg -i ' + file + file_extension + ' ' + file + '.wav'
+		final_audio = 'lame '+ file + '.wav' + ' ' + file + '.mp3'
+		os.system(video_to_wav)
+		os.system(final_audio)
+		#file=pipes.quote(file)
+		#os.remove(file + '.wav')
+		print("sucessfully converted ", fileName, " into audio!")
+	except OSError as err:
+		print(err.reason)
+		exit(1)
+
+
+filenamerandom = str(random.randint(1, 10000))
+yt = input("What video URL?")
+print("Fetching Video.")
+
+youtube = etree.HTML(urllib.request.urlopen(yt).read())
+video_title = youtube.xpath("//span[@id='eow-title']/@title")
+print(''.join(video_title))
+title = ''.join(video_title)
+
+
+paths = os.path.dirname(sys.argv[0])
+path = os.path.abspath(paths) + "\\"
+
+
+
+import pafy
+video = pafy.new(yt)
+bestaudio = video.getbestaudio()
+bestaudio.bitrate #get bit rate
+bestaudio.extension #extension of audio fileurl
+...
+bestaudio.url #get url
+...
+
+bestaudio.download(filenamerandom + ".mp4")
+
+
+
+print("Converting video to audio")
+video_to_audio(filenamerandom + ".mp4")
+print("Done")
+
+
+
+
+AUDIO_FILE = filenamerandom + ".wav"
+
+# use the audio file as the audio source                                        
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source)  # read the entire audio file                  
+
+        print("Transcription: " + r.recognize_google(audio))
+
